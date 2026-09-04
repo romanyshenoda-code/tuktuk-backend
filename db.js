d4a1079c6129ab7ca836ef9439ebb0e7e2ed6708
romanyshenoda-code <romanyshenoda12@gmail.com>
2026-09-04 05:06:38 +0300

    استخدام الشبكة الداخلية للاتصال بقاعدة البيانات

--- db.js
+++ db.js
@@ -6,9 +6,7 @@ const connection = mysql.createConnection({
   port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  connectTimeout: 20000,
-  family: 4
+  database: process.env.DB_NAME
 });
 
 connection.on('error', (err) => {
