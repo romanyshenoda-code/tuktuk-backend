92bb6f5936ff85afd8bf69d9908599d376c4c17a
romanyshenoda-code <romanyshenoda12@gmail.com>
2026-09-04 04:23:21 +0300

    إضافة SSL للاتصال بقاعدة البيانات

--- db.js
+++ db.js
@@ -6,7 +6,10 @@ const connection = mysql.createConnection({
   port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 connection.connect((err) => {
