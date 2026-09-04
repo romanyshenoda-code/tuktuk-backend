d4ff9768f185383f8a82bb51b8e5e1e41f7d01ac
romanyshenoda-code <romanyshenoda12@gmail.com>
2026-09-04 04:55:43 +0300

    استخدام connection URI للاتصال بقاعدة البيانات

--- db.js
+++ db.js
@@ -2,12 +2,7 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 3306,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  connectTimeout: 20000
+  uri: `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
 });
 
 connection.on('error', (err) => {
