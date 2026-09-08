2083b9b082d34b0ee7e35be493a42149c4a66a40
romanyshenoda-code <romanyshenoda12@gmail.com>
2026-09-08 05:22:11 +0300

    تصحيح فرق التوقيت الى 3 ساعات

--- db.js
+++ db.js
@@ -10,7 +10,7 @@ const pool = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  timezone: '+03:00'
+  timezone: 'Africa/Cairo'
 });
 
 pool.getConnection((err, connection) => {
